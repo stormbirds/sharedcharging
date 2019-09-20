c9d0d4964177f3381e0846415d3e23e0aba078cf
stormbirds <xbaojun@gmail.com>
2019-09-20 20:31:08 +0800

    增加nacos及dubbo

--- web/src/main/java/cn/stormbirds/sharedcharging/web/WebApplication.java
+++ web/src/main/java/cn/stormbirds/sharedcharging/web/WebApplication.java
@@ -1,6 +1,7 @@
 package cn.stormbirds.sharedcharging.web;
 
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
